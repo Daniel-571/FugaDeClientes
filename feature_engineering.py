c4a985ab788563e9c9309cc1e4435525307d5ffa
Daniel-571 <olveradaniel477@gmail.com>
2025-11-26 16:43:41 +0100

    Primer commit

--- feature_engineering.py
+++ feature_engineering.py
@@ -1,4 +1,4 @@
-# Feature Engineering y Codificación
+# Feature Engineering y codificación
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
